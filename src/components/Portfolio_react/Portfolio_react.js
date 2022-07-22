73fa7c7207201b4695e6b24c22bb793da8d6bed9
PawelPazderski <pawel.pazderski@op.pl>
2022-07-22 22:25:24 +0200

    not showing page button to desktop pages while mobile

--- src/components/Portfolio_react/Portfolio_react.js
+++ src/components/Portfolio_react/Portfolio_react.js
@@ -59,7 +59,7 @@ const Portfolio_react = () => {
                                 
                                 <h6>{item.technologies}</h6>
                                 <div className="react-links">
-                                    <a className="portfolio-react-link" target="_blank" rel="noreferrer" href={item.page}>Strona</a>
+                                    { (!item.title.includes('desktop')) && <a className="portfolio-react-link" target="_blank" rel="noreferrer" href={item.page}>Strona</a>}
                                     <a className="portfolio-react-link" target="_blank" rel="noreferrer" href={item.link}>Kod</a>
                                 </div>
                                 <hr/>
