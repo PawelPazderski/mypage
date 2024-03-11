2d87d62dd67fa335b2bda00e113a2b7c1d9d9379
PawelPazderski <pazderski.projekt@gmail.com>
2024-03-11 19:45:24 +0100

    styling improvement

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,12 +12,12 @@ const Home = () => {
             <div className='home-container-foto'></div>
             <div className='home-container-text'>
                 <h1>Witaj na mojej stronie!</h1>
-                <p>Od ponad 6 lat pracuję jako Projektant graficzny, zajmując się zarówno obsługą klientów indywidualnych, jak też zadaniami dużej drukarni. Zajmuję się brandingiem, projektami folderów, katalogów, opakowań i wielu innych materiałów do druku i nie tylko. Mam również przyjemność być grafikiem prowadzącym ostatnich ośmiu wydań magazynu <a href="https://osobowosciisukcesy.pl/aktualny-numer-online/" target="blank">Osobowości i Sukcesy</a>.</p>
+                <p>Pracuję jako Projektant graficzny od ponad 6 lat, zajmując się zarówno obsługą klientów indywidualnych, jak też zadaniami dużej drukarni. Zajmuję się brandingiem, projektami folderów, katalogów, opakowań i wielu innych materiałów do druku i nie tylko. Mam również przyjemność być grafikiem prowadzącym ostatnich ośmiu wydań magazynu <a href="https://osobowosciisukcesy.pl/aktualny-numer-online/" target="blank">Osobowości i Sukcesy</a>.</p>
 
                 {/* <p>Wcześniej wiele lat byłem związany z branżą gastronomiczną, prowadząc własną działalność. Specyfika rynku, trudności jakich w branży nie brakuje a także, może przede wszystkim fakt pojawienia się w moim życiu moich cudownych synków skłoniły mnie do poszukiwań nowej drogi zawodowej. Przemieniłem wówczas swoje hobby, jakim 
                 była grafika komputerowa w pracę zawodową, kończąc dwuletnią Szkołę Sztuk Pięknych jako plastyk - specjalność techniki graficzne.</p> */}
 
-                <p>Od dłuższego czasu interesuje mnie też IT - szukam nowych wyzwań i przede wszystkim możliwości rozwoju zawodowego. Pierwsze kroki stawiałem na kursie Frontend Developer - React. Udział w projekcie stażowym Intive 2023 dał mi możliwość spróbowania swoich sił w zespole frontendowym dużego projektu i poznania nowych technologii. </p>
+                <p>Od dłuższego czasu interesuje mnie też IT - szukam nowych wyzwań i przede wszystkim możliwości rozwoju zawodowego. Pierwsze kroki stawiałem na kursie Frontend Developer - React w CodersLab. Udział w ogólnopolskim projekcie Intive Patronage 2023 dał mi możliwość spróbowania swoich sił w zespole frontendowym dużego projektu i poznania nowych technologii. </p>
 
                 <a href="./portfolio" className='strong-text'>Zapraszam serdecznie do obejrzenia moich prac i kontaktu.</a>
             </div>
